52a38fcd3991288b44712c0dc1f4ace5f4528429
hasoro <noily4748@gmail.com>
2025-01-20 17:53:25 +0900

    first commit

---
+++ src/test/java/com/nungil/NungilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nungil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NungilBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
