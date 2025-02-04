3b819da807972975e88ea4c81dc9c9ca6cfae1b0
SeoHong23 <ghd2408@naver.com>
2025-02-04 09:56:47 +0900

    크롤링2

--- src/main/java/com/nungil/NungilBackendApplication.java
+++ src/main/java/com/nungil/NungilBackendApplication.java
@@ -3,7 +3,9 @@ package com.nungil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication(scanBasePackages = {"com.nungil", "config"}) // config 패키지 스캔
 public class NungilBackendApplication {
 
