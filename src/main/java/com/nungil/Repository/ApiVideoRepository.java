b491c4ded4a570da1ab4ba26390640633e8b0994
Jptrnmrs <jptrnmrs@gmail.com>
2025-02-05 11:56:18 +0900

    코드 수정

--- src/main/java/com/nungil/Repository/ApiVideoRepository.java
+++ src/main/java/com/nungil/Repository/ApiVideoRepository.java
@@ -4,7 +4,4 @@ import com.nungil.Document.VideoDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ApiVideoRepository extends MongoRepository<VideoDocument,Long> {
-    boolean existsByCommCode(String commCode); // commCode가 존재하는지 확인하는 메서드
-
-    int findAllCommCodes();
 }
