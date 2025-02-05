39f58b1dc8d675a367f2d12e082add4c873821b5
dnjsrldus01 <dnjsrldus01@gmail.com>
2025-02-05 15:16:08 +0900

    user

--- src/main/java/com/nungil/Repository/Interfaces/LikesRepository.java
+++ src/main/java/com/nungil/Repository/Interfaces/LikesRepository.java
@@ -10,4 +10,6 @@ public interface LikesRepository {
     int insertLike(@Param("videoId") String videoId, @Param("userId") Long userId);
 
     int existsLike(@Param("videoId") String videoId, @Param("userId") Long userId);
+
+    void deleteLike(@Param("videoId") String videoId, @Param("userId") Long userId);
 }
