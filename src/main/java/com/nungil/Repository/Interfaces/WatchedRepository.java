ded05d0136fef16d9e2b7e18e5c7fa59e75bbd9d
dnjsrldus01 <dnjsrldus01@gmail.com>
2025-02-10 17:20:21 +0900

    user

--- src/main/java/com/nungil/Repository/Interfaces/WatchedRepository.java
+++ src/main/java/com/nungil/Repository/Interfaces/WatchedRepository.java
@@ -2,9 +2,14 @@ package com.nungil.Repository.Interfaces;
 
 import com.nungil.Dto.WatchedDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface WatchedRepository {
 
     void insertWatched(WatchedDTO watchedDTO);
+
+    void deleteWatched(WatchedDTO watchedDTO);
+
+    boolean existsByUserIdAndVideoId(@Param("userId") Long userId, @Param("videoId") String videoId);
 }
