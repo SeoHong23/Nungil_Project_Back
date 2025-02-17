24d459df28f1e89308e3c04f4c214775ee2ce35a
hasoro <noily4748@gmail.com>
2025-02-17 10:19:56 +0900

    admin값 수정

--- src/main/java/com/nungil/Dto/UserDTO.java
+++ src/main/java/com/nungil/Dto/UserDTO.java
@@ -20,13 +20,15 @@ public class UserDTO {
     private String email;        // 이메일
     private int birthDate;    // 생년월일
     private Gender gender;       // 성별
+    private boolean admin;
 
-    public UserDTO(int userid, String password, String nickname, String email, int birthDate, Gender gender) {
+    public UserDTO(int userid, String password, String nickname, String email, int birthDate, Gender gender,boolean admin) {
         this.userid = userid;
         this.password = password;
         this.nickname = nickname;
         this.email = email;
         this.birthDate = birthDate;
         this.gender = gender;
+        this.admin = admin;
     }
 }
--- src/main/java/com/nungil/entity/UserEntity.java
+++ src/main/java/com/nungil/entity/UserEntity.java
@@ -2,10 +2,12 @@ package com.nungil.entity;
 
 
 import com.nungil.Enum.Gender;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.simpleframework.xml.Default;
 
 @Entity
 @Table(name = "users")
@@ -23,13 +25,16 @@ public class UserEntity {
     private String email;        // 이메일
     private int birthDate;    // 생년월일
     private Gender gender;       // 성별
+    @Column(columnDefinition = "TINYINT(1)")
+    private boolean admin;
 
-    public UserEntity(String password, String nickname, String email, int birthDate, Gender gender) {
+    public UserEntity(String password, String nickname, String email, int birthDate, Gender gender, boolean admin) {
         this.password = password;
         this.nickname = nickname;
         this.email = email;
         this.birthDate = birthDate;
         this.gender = gender;
+        this.admin = admin;
     }
 
 }
