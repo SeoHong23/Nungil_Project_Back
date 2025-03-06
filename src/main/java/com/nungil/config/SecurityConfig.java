9ec51e0653e4806aafa048ec84ba16e593a02510
Jptrnmrs <jptrnmrs@gmail.com>
2025-03-06 17:08:33 +0900

    비밀번호 암호화

--- src/main/java/com/nungil/config/SecurityConfig.java
+++ src/main/java/com/nungil/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +20,9 @@ public class SecurityConfig {
                 .anyRequest().authenticated());  // 인증된 사용자만 접근 가능
 
         return http.build();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
